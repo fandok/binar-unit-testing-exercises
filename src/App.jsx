c939a9d18c4cd2b094081994c9a462577f7f595d
Fandika Okdiba <fandika.okdiba@amartha.com>
2024-02-21 16:27:26 +0700

    add unit test exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+
+import { POKEMON_API } from "./constants";
+
 const App = () => {
-  return <div>App</div>;
+  const [pokemon, setPokemon] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(POKEMON_API);
+      const responseJson = await response.json();
+      setPokemon(responseJson.results);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {value ? value : "App"}
+      <button
+        onClick={() => {
+          setValue("Updated");
+        }}
+      >
+        Click
+      </button>
+      <br />
+      {pokemon.map((poke) => (
+        <div key={poke.name}>{poke.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,21 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { HttpResponse, http } from "msw";
+import { setupServer } from "msw/node";
+import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
 
 import App from "./App";
+import { POKEMON_API } from "./constants";
+import appMock from "./mocks/App.mock";
+
+const server = setupServer(
+  http.get(POKEMON_API, () => {
+    return HttpResponse.json(appMock);
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe("App page", () => {
   it("renders page and get App text", () => {
